bc2d352fbdc2d59a0ced4b9cda51550740cfc5ef
Adrianfer <35809235+Adrianfer20@users.noreply.github.com>
2024-06-28 10:15:15 -0400

    add loading in PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,12 @@
-// src/PrivateRoute.jsx
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
 const PrivateRoute = ({ element: Component, ...rest }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
+  if (loading) return <div>Loading...</div>; // Puedes personalizar el indicador de carga
+  
   return user ? <Component {...rest} /> : <Navigate to="/login" />;
 };
 
